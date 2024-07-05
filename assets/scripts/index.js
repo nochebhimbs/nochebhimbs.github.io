17232aeaad6b90d9974d0d64a0bcea280e4550e5
nochebhimbs <nochebhimbs@gmail.com>
2024-07-05 20:39:46 +0800

    initial commit

---
+++ assets/scripts/index.js
@@ -0,0 +1,10 @@
+const div = document.querySelector("div")
+const spanHi = document.querySelector("#hi")
+const spanHi2 = document.getElementById("hi2")
+const spanBye = document.querySelector("#bye")
+
+spanHi.classList.add("new-class")
+spanHi.classList.remove("hi1")
+
+spanHi.style.color = "red"
+spanHi.style.backgroundColor = "red"
